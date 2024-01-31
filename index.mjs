93cc098d83750810cb337f6a5a9acf96d029a763
Dripa Alexandr <forsyteRun@gmail.com>
2024-01-31 14:56:57 +0200

    feat: implement cd

--- index.mjs
+++ index.mjs
@@ -1,33 +1,49 @@
-import { getUserName, getPathToRoot, createFile } from "./utils/index.mjs"
+import { getUserName, getPathToRoot, createFile, getCurrenDirText } from "./utils/index.mjs"
+import { join, isAbsolute } from 'path'
 
 const userName = getUserName()
 const pathToRoot = getPathToRoot(import.meta.url)
 
+let PATH_TO_CURRENT_DIR = ''
+
 const initText = `Welcome to the File Manager, ${userName}! \n`
 const finalText = `Thank you for using File Manager, ${userName}, goodbye! \n`
-const currenDirText = `You are currently in ${pathToRoot}\n`
 
 process.stdout.write(initText),
-process.stdout.write(currenDirText)
+getCurrenDirText(PATH_TO_CURRENT_DIR || pathToRoot)
+
 
 process.stdin.on('data', async (data) => {
-  process.stdout.write(currenDirText)
   const [work, fileName] = data.toString().trim().split(' ')
   switch (work) {
     case '.exit':
       process.stdout.write(finalText),
       process.stdin.destroy()
       break;
-      
+
+    case 'cd':
+      if (isAbsolute(fileName)) {
+        //добавить проверку на наличие папки
+        PATH_TO_CURRENT_DIR = fileName
+      } else {
+        PATH_TO_CURRENT_DIR = join(PATH_TO_CURRENT_DIR || pathToRoot, fileName)
+      }
+
+      getCurrenDirText(PATH_TO_CURRENT_DIR)
+    break;
+        
     case 'add':
       try {
-       await createFile(pathToRoot, fileName)
+        await createFile(PATH_TO_CURRENT_DIR || pathToRoot, fileName)
+        getCurrenDirText(PATH_TO_CURRENT_DIR || pathToRoot)
       } catch (error) {
-        
+        throw new Error(error)
       }
-      break;
-      
-    default:
     break;
-  }
+    
+    default:
+      break;
+    }
+  // console.log(PATH_TO_CURRENT_DIR, 11);
+  // process.stdout.write(currenDirText)
 })
--- utils/index.mjs
+++ utils/index.mjs
@@ -1,6 +1,7 @@
 import  {createFile}  from './createFile.mjs'
 import  {getPathToRoot}  from './getPathToRoot.mjs'
 import {getUserName}  from './getUserName.mjs' 
+import {getCurrenDirText}  from './getCurrenDirText.mjs' 
 
-export {createFile, getPathToRoot, getUserName}
+export {createFile, getPathToRoot, getUserName, getCurrenDirText}
 
